41ca595eeb6001a0d49316347879ee62f56990ce
rupu1906 <rupu1906@gmail.com>
2022-04-05 10:18:12 -0400

    Update test_search.py

--- tests/ui/test_search.py
+++ tests/ui/test_search.py
@@ -18,8 +18,3 @@ class TestWeb(WebBaseClass):
     @allure.title("Home page - smoke test")
     def test_homepage_title(self):
         self.driver.get("https://www.google.com")
-
-    @allure.title("Home page - smoke test")
-    def test_homepage_title(self):
-        self.driver.get("https://www.google.com")
-        assert False
