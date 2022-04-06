b111b4f16751f5a8e13ae36ce73157f073bbe271
rupu1906 <rupu1906@gmail.com>
2022-04-06 16:52:51 -0400

    Create test_demo.py

---
+++ tests/ui/test_demo.py
@@ -0,0 +1,20 @@
+from src.PracticeTests.utils.logger import logger
+
+
+
+
+class TestWeb(WebBaseClass):
+
+    def test_homepage_title(self):
+        self.driver.get("https://www.google.com")
+
+
+    def test_homepage_title1(self):
+        self.driver.get("https://www.google.com")
+
+
+    def test_homepage_title12(self):
+        self.driver.get("https://www.google.com")
+
+    def test_homepage_title13(self):
+        self.driver.get("https://www.google.com")
